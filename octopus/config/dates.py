77bb4d836d78fb4bac82178eadb24d6cdb43cdfe
Anusha Ranganathan <anusha@cottagelabs.com>
2025-05-11 17:33:08 +0530

    Expand date parding to use dateutils parser

--- octopus/config/dates.py
+++ octopus/config/dates.py
@@ -5,7 +5,6 @@ DEFAULT_DATE_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
 # date formats that we know about, and should try, in order, when parsing
 DATE_FORMATS = [
     "%Y-%m-%dT%H:%M:%SZ",   # e.g. 2014-09-23T11:30:45Z
-    "%Y-%m-%dT%H:%M:%S",  # e.g. 2014-09-23T11:30:45Z
     "%Y-%m-%d",             # e.g. 2014-09-23
     "%d/%m/%y",             # e.g. 29/02/80
     "%d/%m/%Y",             # e.g. 29/02/1980
